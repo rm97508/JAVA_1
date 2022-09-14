8cba5714ee58d195bf6dc272d11986394efb66a2
rm97508 <112440646+rm97508@users.noreply.github.com>
2022-09-13 22:23:38 -0300

    Update Ex44.java

--- Ex44.java
+++ Ex44.java
@@ -1,10 +1,36 @@
-package entrada_processamento_saida;
+import java.util.Scanner;
 
-public class ex44 {
+public class Ex44F {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in);
+		
+		double i, soma, n, maior, media;
+		
+		soma = 0;
+		maior = 0;
+		
+		for (i=1; (i <= 10); i++) {
+			System.out.println("Digite o " + (int) i + "º número positivo: ");
+			n = ler.nextDouble();
+			
+			while (n < 0) {
+				System.out.println("O número deve ser positivo!");
+				System.out.println("Digite o " + (int) i + " número positivo: ");
+				n = ler.nextDouble();
+			}if (i == 1) {
+				maior = n;
+			}else if (n > maior) {
+				maior = n;
+			}
+			
+			soma = soma + n;
+            ler.close();
+		}
+		media = soma / (i - 1);
+		
+		System.out.println("O maior número é: " + maior);
+		System.out.println("A soma dos valores é: " + soma);
+		System.out.println("A média dos valores é: " + media);
 	}
-
 }
