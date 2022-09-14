817eda07c739e8b5dd88920a7aa5ea25cd618b98
rm97508 <112440646+rm97508@users.noreply.github.com>
2022-09-13 22:25:15 -0300

    Update Ex42.java

--- Ex42.java
+++ Ex42.java
@@ -1,10 +1,36 @@
-package entrada_processamento_saida;
+import java.util.Scanner;
 
-public class ex42 {
+public class Ex42W {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in);
+		
+		int n, i;
+		double dividendo, divisor, divisao, soma;
+		
+		System.out.println("Digite o termo que quer encontrar (valor deve ser positivo e menor que 50): ");
+		n = ler.nextInt();
+		
+		while ((n <= 0) || (n > 50)) {
+			System.out.println("O valor de N deve ser positivo e menor que 50.");
+			System.out.println("Digite o termo que quer encontrar (valor deve ser positivo e menor que 50): ");
+			n = ler.nextInt();
+		}
+		
+		dividendo = 1;
+		divisor = 2;
+		i = 1;
+		soma = 0;
+		
+		while (i <= n) {
+		    divisao = dividendo / divisor;
+		    soma = soma + divisao;
+    	    System.out.println(i + ". " + (int) dividendo + "/" + (int) divisor);
+    	    dividendo++;
+    	    divisor++;
+    	    i++;
+		}
+		System.out.printf("A soma é: %.2f", soma);
+        ler.close();
 	}
-
 }
