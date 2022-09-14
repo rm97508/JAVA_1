cdb459d5b38bfb6597805921da2331c574b7c396
rm97508 <112440646+rm97508@users.noreply.github.com>
2022-09-13 22:30:26 -0300

    Update Ex36.java

--- Ex36.java
+++ Ex36.java
@@ -1,10 +1,31 @@
-package entrada_processamento_saida;
+import java.util.Scanner;
 
-public class ex36 {
+public class Ex36DW {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-	}
+        int ct = 1;
+        int tt = 1;
+        int v = 0;
 
+         do {
+            System.out.print("Entre com um valor: ");
+            v = entrada.nextInt();
+        }while (v < 0);
+
+        System.out.print("Entre com começo da tabuada: ");
+        ct = entrada.nextInt();
+
+        do {
+            System.out.print("Entre com o final da tabuada:");
+                tt = entrada.nextInt();
+        } while (ct > tt);
+
+        do {
+            System.out.println(v * tt);
+            tt--;
+        } while (tt >= ct);
+        entrada.close();
+    }
 }
