0a18c74301379b0e439c396de67dc8477c6c9ae8
rm97508 <112440646+rm97508@users.noreply.github.com>
2022-09-13 22:29:28 -0300

    Update Ex37.java

--- Ex37.java
+++ Ex37.java
@@ -1,15 +1,27 @@
-package entrada_processamento_saida;
-
 import java.util.Scanner;
 
-public class ex37 {
+public class Ex37DW {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner ler = new Scanner (System.in);
-		
-		
+    public static void main(String[] args) {
 
-	}
+    Scanner entrada = new Scanner(System.in);
+    
+    int i = 1;
+    int j = 1;
 
+    do {
+        do {
+            System.out.println(i + " x " + j + " = " + (i * j));
+            j++;
+        } while (j <= 10);
+
+        System.out.print("Digite algo para continuar ");
+        entrada.next();
+
+        i++;
+        j = 1;
+    } while (i <= 20);
+    entrada.close();
+
+    }
 }
