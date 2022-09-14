c0709dbbeddbe323a60e5b27d2259ef518cdd7ec
rm97508 <112440646+rm97508@users.noreply.github.com>
2022-09-13 22:31:44 -0300

    Update Ex35.java

--- Ex35.java
+++ Ex35.java
@@ -1,10 +1,33 @@
-package entrada_processamento_saida;
+import java.util.Scanner;
 
-public class ex35 {
+public class Ex35W {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+            int i = 1;
+            int j = 1;
+            int valor = 0;
 
-	}
+            Scanner entrada = new Scanner(System.in);
+            System.out.print("Entre com um valor: ");
+            valor = entrada.nextInt();
 
+            while(valor < 0){
+                System.out.print("Entre com um valor: ");
+                valor = entrada.nextInt();
+
+            }
+
+            // Processamento
+           while(i <= valor){
+            j = 1;
+            while(j <= 10){
+                System.out.println(i + " x " + j + " = " + (i * j));
+                j++;
+            }
+
+            i++;
+           }
+           entrada.close();
+
+    }
 }
