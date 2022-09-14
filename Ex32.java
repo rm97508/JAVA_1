a606d86446931f00876347c42ac876539230ed72
rm97508 <112440646+rm97508@users.noreply.github.com>
2022-09-13 22:18:06 -0300

    Update Ex32.java

--- Ex32.java
+++ Ex32.java
@@ -1,10 +1,28 @@
-package entrada_processamento_saida;
+import java.util.Scanner;
 
 public class Ex32 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		
+		double av, bv;
+		
+		System.out.println("Digite o primeiro valor: ");
+		av = ler.nextDouble();
+		
+		System.out.println("Digite o segundo valor: ");
+		bv = ler.nextDouble();
+		
+		while (bv <= av) {
+			System.out.println("O segundo valor digitado deve ser maior do que o primeiro");
+			System.out.println("Digite o segundo valor (deve ser maior do que o primeiro): ");
+			bv = ler.nextDouble();
+		}
+		
+		System.out.println("O segundo valor digitado (" + bv + ") é maior do que o primeiro (" + av +")");
 
+        ler.close();
 	}
+    
 
 }
